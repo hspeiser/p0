425af5da7efe1542fd19d93a2ca41e15801dbacb
BrokenKeyboard <satasiyahet@gmail.com>
2025-12-24 13:21:35 -0800

    geeked

--- examples/simulation/sim.py
+++ examples/simulation/sim.py
@@ -7,7 +7,7 @@ joints = ["Pan", "Proximal", "Distal", "Wrist","Roll", "Gripper"]
 kinematics = rk("../../rerun_arm/robot.urdf", "jaw_base", joints)
 positions = np.array([0, 0, 0, 0, 0])
 t_des = np.eye(4, dtype=float)
-t_des[:3, :3] = Rotation.from_rotvec(np.array([3.14/2.0, 3.14/2.0, 3.14/2.0],  dtype=float)).as_matrix()
+t_des[:3, :3] = Rotation.from_rotvec(np.array([0, .14/2.30, 3.14/2.0],  dtype=float)).as_matrix()
 t_des[:3, 3] = np.array([0, 0, 0.2], dtype=float)
 thing2 = kinematics.inverse_kinematics(positions, t_des)
 print(thing2)
@@ -22,9 +22,11 @@ args = parser.parse_args()
 with socket(AF_INET, SOCK_DGRAM) as client_socket:
     # send message to server
     print(f'Sending message to {"localhost"}:{9999}')
-    # for x in range(-90, 90):
-    message = {}
-    for thing in range(0,6):
-        message[joints[thing]] = thing2[thing]
-    client_socket.sendto(json.dumps(message).encode(), ("localhost", 9999))
+    joint = input("Joint to sweep")
+    for x in range(-90, 90):
+        client_socket.sendto(json.dumps({joint: x}).encode(), ("localhost", 9999))
+    # message = {}
+    # for thing in range(0,6):
+    #     message[joints[thing]] = thing2[thing]
+    # client_socket.sendto(json.dumps(message).encode(), ("localhost", 9999))
     
